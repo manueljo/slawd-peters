8d9e1a0a51d966f934db2c20434d21c67c1f2d7e
manueljo <mikebillyy@gmail.com>
2025-08-21 17:27:34 +0100

    Add link to logo

--- components/ui/resizable-navbar.jsx
+++ components/ui/resizable-navbar.jsx
@@ -194,7 +194,7 @@ export const MobileNavToggle = ({
 export const NavbarLogo = () => {
   return (
     (<Link
-      href="#"
+      href="/"
       className="relative z-20 mr-4 flex items-center space-x-2 px-2 py-1 text-sm font-normal text-black">
       <Image
         src="/images/slawd_logo.png"
